a8a28c5704d13801238ffec09b350217dc2484df
Giovanni Lodi <ing.giovanni.lodi@gmail.com>
2013-01-07 01:56:21 +0000

    Completed method to produce a dict of colors info out of the web page

--- colors_spider.py
+++ colors_spider.py
@@ -15,6 +15,15 @@ import urllib
 from bs4 import BeautifulSoup
 
 
+def rgb_from_hex(hex_color):
+    hex_color = hex_color.replace("#", "")
+    red = int(hex_color[0:2], 16)
+    green = int(hex_color[2:4], 16)
+    blue = int(hex_color[4:6], 16)
+
+    return dict(red=red, green=green, blue=blue)
+
+
 def fetch_a_page_from_the_web(url_string):
     # get a file-like object form the page
     page = urllib.urlopen(url_string)
@@ -27,15 +36,29 @@ def fetch_a_page_from_the_web(url_string):
     return page_content
 
 
-def fetch_colors_info_node(page_content):
-
+def fetch_colors_info(page_content):
     soup = BeautifulSoup(page_content)
 
     # print soup.prettify()
 
-    return soup.find("table", "reference")
+    table = soup.find("table", "reference")
+    trs = table.findChildren('tr')
+
+    colors = []
+    for tr in trs:
+        tds = tr.findChildren('td')
+
+        if len(tds) > 0:
+            color_name = tds[0].find("a").string
+            color_hex = tds[1].find("a").string
+
+            color_dict = dict(name=color_name, components=rgb_from_hex(color_hex))
+
+            colors.append(color_dict)
+
+    print colors
 
 
 url_string = "http://www.w3schools.com/cssref/css_colornames.asp"
 page_content = fetch_a_page_from_the_web(url_string)
-colors_info = fetch_colors_info_node(page_content)
+colors_info = fetch_colors_info(page_content)
