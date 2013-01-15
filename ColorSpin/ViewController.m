f428ef8682ffdc52b60e71e3c47cb5e7a061995f
Giovanni Lodi <ing.giovanni.lodi@gmail.com>
2013-01-15 23:30:50 +0000

    Some stuff to test our new shiny library of colors

--- ColorSpin/ViewController.m
+++ ColorSpin/ViewController.m
@@ -7,9 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "UIColor+CSSColors.h"
 
 @interface ViewController ()
-
+- (void)spinColor;
+- (UIColor *)getRandomColor;
 @end
 
 @implementation ViewController
@@ -17,7 +19,11 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+
+    UITapGestureRecognizer *tapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(spinColor)];
+    tapRecognizer.numberOfTapsRequired = 1;
+    
+    [self.view addGestureRecognizer:tapRecognizer];
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +31,45 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (void)spinColor
+{
+    [self.view setBackgroundColor:[self getRandomColor]];
+}
+
+- (UIColor *)getRandomColor
+{
+    int random = arc4random() % 6;
+    
+    UIColor *color = nil;
+    
+    switch (random) {
+        case 0:
+            color = [UIColor lightSeaGreenColor];
+            break;
+        case 1:
+            // dunno why, but indianRedColor reminds me of Assassin's Creed Brotherhood
+            color = [UIColor indianRedColor];
+            break;
+        case 2:
+            color = [UIColor yellowGreenColor];
+            break;
+        case 3:
+            color = [UIColor gainsboroColor];
+            break;
+        case 4:
+            color = [UIColor midnightBlueColor];
+            break;
+        case 5:
+            color = [UIColor orchidColor];
+            break;
+        default:
+            color = [UIColor ghostWhiteColor];
+            break;
+    }
+    
+    return color;
+}
+
 
 @end
